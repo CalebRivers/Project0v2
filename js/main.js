b98a6f5ea6b358431b0bb8bb0018ebbcf25ad1bf
Caleb Rivers <cal1riv@gmail.com>
2018-06-26 11:49:26 +1000

    added styling and js

--- js/main.js
+++ js/main.js
@@ -34,6 +34,12 @@ const isWin = function () {
   }
 }
 
+const isDraw = function () {
+  if ((board[0] === "X" || board[0] === "O" )&& (board[1] === "X" || board[1] === "O") && (board[2] === "X" || board[2] === "O") && (board[3] === "X" || board[3] === "O") && (board[4] === "X" || board[4] === "O") && (board[5] === "X" || board[5] === "O") && (board[6] === "X" || board[6] === "O") && (board[7] === "X" || board[7] === "O") && (board[8] === "X" || board[8] === "O")) {
+    console.log("Its a draw")
+  }
+}
+
 const placeMarker = function (boxNumber) {
   if (counter%2 === 0) {
     board[boxNumber] = 'X';
@@ -43,6 +49,7 @@ const placeMarker = function (boxNumber) {
     counter = counter + 1;
   }
   isWin()
+  isDraw()
   console.log(board);
 }
 
@@ -60,5 +67,39 @@ $(document).ready(function () {
   $("#box0").on('click', function () {
     placeMarker(0);
     placePic(0);
+  })
+
+  $("#box1").on('click', function () {
+    placeMarker(1);
+    placePic(1);
+  })
+
+  $("#box2").on('click', function () {
+    placeMarker(2);
+    placePic(2);
+  })
+  $("#box3").on('click', function () {
+    placeMarker(3);
+    placePic(3);
+  })
+  $("#box4").on('click', function () {
+    placeMarker(4);
+    placePic(4);
+  })
+  $("#box5").on('click', function () {
+    placeMarker(5);
+    placePic(5);
+  })
+  $("#box6").on('click', function () {
+    placeMarker(6);
+    placePic(6);
+  })
+  $("#box7").on('click', function () {
+    placeMarker(7);
+    placePic(7);
+  })
+  $("#box8").on('click', function () {
+    placeMarker(8);
+    placePic(8);
   })
 })
