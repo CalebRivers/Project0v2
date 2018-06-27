4436fdf23026391345a7b96c404832f2b8156d42
Caleb Rivers <cal1riv@gmail.com>
2018-06-28 09:03:29 +1000

    added the draw function

--- js/main.js
+++ js/main.js
@@ -36,7 +36,8 @@ const isWin = function () {
 
 const isDraw = function () {
   if ((board[0] === "X" || board[0] === "O" )&& (board[1] === "X" || board[1] === "O") && (board[2] === "X" || board[2] === "O") && (board[3] === "X" || board[3] === "O") && (board[4] === "X" || board[4] === "O") && (board[5] === "X" || board[5] === "O") && (board[6] === "X" || board[6] === "O") && (board[7] === "X" || board[7] === "O") && (board[8] === "X" || board[8] === "O")) {
-    console.log("Its a draw")
+    // console.log("Its a draw")
+    $(".draw").attr('id','gameOverShow');
   }
 }
 
